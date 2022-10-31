51942d79dd12506e0ce97de8f245298ab3a35cd5
Ananya1516 <96481824+Ananya1516@users.noreply.github.com>
2022-10-31 14:49:36 +0530

    def getpf(num):     s=0     for i in range(1,num):         if num%i==0:             s=s+i     return s a=int(input()) b=int(input()) if getpf(a)==b and getpf(b)==a:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,15 +1,12 @@
+def getpf(num):
+    s=0
+    for i in range(1,num):
+        if num%i==0:
+            s=s+i
+    return s
 a=int(input())
 b=int(input())
-pf_a=0
-for i in range(1,a):
-    if a%i==0:
-        pf_a+=i
-pf_b=0
-for i in range(1,b):
-    if b%i==0:
-        pf_b+=i
-if pf_a == b and pf_b == a:
+if getpf(a)==b and getpf(b)==a:
     print("Amicable")
 else:
     print("Not Amicable")
-       
